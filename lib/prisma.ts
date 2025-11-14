2cf9999288068a7caa92213b7d53106f8bbb426e
Temi-pinheiro <temitopepinheiro@gmail.com>
2025-11-14 01:17:02 +0100

    remigrate to prisma

---
+++ lib/prisma.ts
@@ -0,0 +1,25 @@
+import { PrismaPg } from '@prisma/adapter-pg';
+
+import { PrismaClient } from './generated/prisma/client';
+
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const adapter = new PrismaPg({ connectionString });
+
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    adapter,
+    log:
+      process.env.NODE_ENV === 'development'
+        ? ['query', 'error', 'warn']
+        : ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
--- src/app/api/waitlist/route.js
+++ src/app/api/waitlist/route.js
@@ -1,41 +1,36 @@
 import { NextResponse } from 'next/server';
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY // use service key here only in server routes
-);
+import prisma from '../../../../lib/prisma';
 
 export async function POST(req) {
-  const { email } = await req.json();
+  try {
+    const { email } = await req.json();
+    console.log(email);
 
-  let errorMessage;
+    await prisma.waitlist.create({
+      data: {
+        email,
+      },
+    });
 
-  const { data, error } = await supabase
-    .from('waitlist')
-    .insert([{ email }])
-    .select();
-
-  if (error) {
-    if (error.message.includes('duplicate key')) {
+    return NextResponse.json({ success: true });
+  } catch (error) {
+    console.error('Error adding to waitlist:', error);
+    let errorMessage = 'An unexpected error occurred.';
+    if (error.message.includes('Unique constraint')) {
       errorMessage = "You're already on the list!";
     } else {
       errorMessage = error.message;
     }
     return NextResponse.json({ error: errorMessage }, { status: 400 });
   }
-
-  return NextResponse.json({ success: true, user: data[0] });
 }
 
 export async function GET() {
-  const data = await supabase.from('waitlist_count').select('total').single();
+  try {
+    const data = await prisma.waitlist.count();
 
-  console.log(data.data);
-
-  if (data.error) {
-    return NextResponse.json({ error: data.error?.message }, { status: 400 });
+    return NextResponse.json({ success: true, count: data });
+  } catch (error) {
+    return NextResponse.json({ error: error?.message }, { status: 400 });
   }
-
-  return NextResponse.json({ success: true, count: data.data.total });
 }
